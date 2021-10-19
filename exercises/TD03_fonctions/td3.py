1ee73f164220312ce0b4027d934f9539d1cf27d3
Eyléa PIOUCEAU <eylea.piouceau@ens.uvsq.fr>
2021-10-19 17:19:57 +0200

    TD 3 - 1

---
+++ exercises/TD03_fonctions/td3.py
@@ -0,0 +1,8 @@
+def tempsEnSeconde(temps):
+    '''Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde'''
+    temps = temps[0] * 86400 + temps[1] * 3600 + temps[2] * 60 + temps[3]
+    return temps
+
+temps = (3,23,1,34)
+print(type(temps))
+print(tempsEnSeconde(temps))
