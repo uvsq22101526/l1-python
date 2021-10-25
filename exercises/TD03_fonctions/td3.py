600dd1dd85888828c515e4b144303dc7d22766a5
Eyléa PIOUCEAU <eylea.piouceau@ens.uvsq.fr>
2021-10-25 19:19:35 +0200

    correction TD 1 + TD 2

--- exercises/TD03_fonctions/td3.py
+++ exercises/TD03_fonctions/td3.py
@@ -3,6 +3,7 @@ def tempsEnSeconde(temps):
     temps = temps[0] * 86400 + temps[1] * 3600 + temps[2] * 60 + temps[3]
     return temps
 
-temps = (3,23,1,34)
+
+temps = (3, 23, 1, 34)
 print(type(temps))
 print(tempsEnSeconde(temps))
