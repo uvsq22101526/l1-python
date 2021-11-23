dd6bee2284f0a9c34ff6b5b1ebfdd6167844551a
Eyléa Piouceau <eylea.piouceau@gmail.com>
2021-11-23 17:17:30 +0100

    TD 6

---
+++ exercises/TD04_listes/carre_magique.py
@@ -0,0 +1,100 @@
+'''Créer une liste à deux dimensions affectée à  la variable `carre_mag` contenant ce carré magique.'''
+
+carre_mag = [[4, 14, 15, 1], [9, 7, 6, 12], [5, 11, 10, 8], [16, 2, 3, 13]]
+
+#carre_pas_mag = [[4, 14, 15, 1], [9, 7, 6, 12], [5, 11, 10, 8], [16, 2, 7, 13]]
+carre_pas_mag = [list(i) for i in carre_mag]
+
+def afficheCarre(carre):
+    """ Affiche la liste à 2 dimensions carre comme un carré"""
+    
+    return print(carre[0]), print(carre[1]), print(carre[2]), print(carre[3])
+
+#afficheCarre(carre_mag)
+#afficheCarre(carre_pas_mag)
+
+
+def testLignesEgales(carre):
+    """ Renvoie la somme des éléments d'une ligne de la liste 2D carre si toutes les lignes ont la même somme, et -1 sinon """
+    
+    somme_0 = carre[0][0] + carre[0][1] + carre[0][2] + carre[0][3]
+    somme_1 = carre[1][0] + carre[1][1] + carre[1][2] + carre[1][3]
+    somme_2 = carre[2][0] + carre[2][1] + carre[2][2] + carre[2][3]
+    somme_3 = carre[3][0] + carre[3][1] + carre[3][2] + carre[3][3]
+    
+    if somme_0 == somme_1 and somme_1 == somme_2 and somme_2 == somme_3:
+        return somme_1
+    else:
+        return -1
+
+#print(testLignesEgales(carre_mag))
+#print(testLignesEgales(carre_pas_mag))
+
+def testColonnesEgales(carre):
+    """ Renvoie la somme des éléments d'une colonne de la liste 2D carre si toutes les colonnes ont la même somme, et -1 sinon """
+    
+    somme_0 = carre[0][0] + carre[1][0] + carre[2][0] + carre[3][0]
+    somme_1 = carre[0][1] + carre[1][1] + carre[2][1] + carre[3][1]
+    somme_2 = carre[0][2] + carre[1][2] + carre[2][2] + carre[3][2]
+    somme_3 = carre[0][3] + carre[1][3] + carre[2][3] + carre[3][3]
+    
+    if somme_0 == somme_1 and somme_1 == somme_2 and somme_2 == somme_3:
+        return somme_1
+    else:
+        return -1
+
+#print(testColonnesEgales(carre_mag))
+#print(testColonnesEgales(carre_pas_mag))
+
+def testDiagonalesEgales(carre):
+    """ Renvoie la somme des éléments d'une diagonale de la liste 2D carre si les 2 diagonales ont la même somme, et -1 sinon """
+    
+    somme_1 = carre[0][0] + carre[1][1] + carre[2][2] + carre[3][3]
+    somme_2 = carre[0][3] + carre[1][2] + carre[2][1] + carre[3][0]
+
+    if somme_1 == somme_2:
+        return somme_1
+    else:
+        return -1
+    
+#print(testDiagonalesEgales(carre_mag))
+#print(testDiagonalesEgales(carre_pas_mag))
+
+
+def estCarreMagique(carre):
+    """ Renvoie True si c'est un carre magique et False sinon"""
+    if testLignesEgales(carre) == testColonnesEgales(carre) and testColonnesEgales(carre) == testDiagonalesEgales(carre):
+        return True
+    
+    else:
+        return False
+
+#print(estCarreMagique(carre_mag))
+#print(estCarreMagique(carre_pas_mag))
+
+
+def estNormal(carre):
+    """ Retourne True si contient toutes les valeurs de 1 à n^2 où n est la taille du carré, et False sinon """
+    somme = 0
+    
+    for i in range(1, 4 * 4 + 1):
+        if i in carre[0]:
+            somme += 1
+            
+        if i in carre[1]:
+            somme += 1
+        
+        if i in carre[2]:
+            somme += 1
+            
+        if i in carre[3]:
+            somme += 1
+            
+    if somme == 4 * 4:
+        return True
+    
+    else:
+        return False
+
+print(estNormal(carre_mag))
+print(estNormal(carre_pas_mag))
--- exercises/TD04_listes/syracuse.py
+++ exercises/TD04_listes/syracuse.py
@@ -11,7 +11,7 @@ def syracuse(n):
 
     return liste
 
-print(syracuse(3))
+#print(syracuse(3))
 
 '''La conjecture de Collatz (ou problème de Syracuse) affirme que, quel que soit l’entier `n` que l’on
 choisisse au départ, on finit toujours par arriver à 1 (ce résultat est une conjecture, c'est-à-dire
@@ -25,9 +25,73 @@ Sinon, c’est qu’on sera entré dans une boucle infinie.'''
 def testeConjecture(n_max):
     """ Teste la conjecture de Collatz pour toutes les valeurs de 2 à n_max """
     
-    for i in range(2, n_max + 2):
-        syracuse(n_max)
+    for i in range(1, n_max + 1):
+        syracuse(i)
 
     return True
 
-print(testeConjecture(10000))
+#print(testeConjecture(10000))
+
+
+'''On appelle "temps de vol" de l’entier `n` le nombre d’étapes pour aller de `n` jusqu’à 1.
+Le temps de vol de 1 est 0, le temps de vol de 3 est 7. Écrire une fonction qui, étant donné un paramètre `n`,
+renvoie son temps de vol.'''
+
+def tempsVol(n):
+    """ Retourne le temps de vol de n """
+    
+    return len(syracuse(n)) - 1
+
+#print("Le temps de vol de", 3, "est", tempsVol(3))
+
+
+'''Ecrire une fonction qui, étant donné un paramètre `n_max` renvoie la liste des temps de vol de tous les entiers de 1 à `n_max`.
+*Indication*: utiliser une liste en compréhension.'''
+
+def tempsVolListe(n_max):
+    """ Retourne la liste de tous les temps de vol de 1 à n_max """
+
+    liste = [tempsVol(i) for i in range(1, n_max + 1)]
+
+    return liste
+        
+#print(tempsVolListe(100))
+
+
+'''Déterminer quel entier entre 1 et 10000 a le plus grand temps de vol (réponse 6171 qui a le temps de vol 261).'''
+
+liste = []
+
+for i in range(1, 10000 + 1):
+    liste.append(tempsVol(i))
+
+
+vol_max = max(tempsVolListe(10000))
+entier_vol_max = liste.index(max(tempsVolListe(10000))) + 1
+
+print("L'entier qui a le plus grand temps de vol est", entier_vol_max, "avec un temps de vol égal à", vol_max)
+
+
+'''L’altitude maximale de l'entier `n` est la plus grande valeur par laquelle passe `n` au cours de son vol.
+Déterminer quel entier entre 1 et 10000 a la plus grande altitude maximale (réponse 27114424, atteint par l'entier 9663).
+Ne pas hésiter à écrire de nouvelles fonctions pour cela.'''
+
+def altitudeMax(n):
+    """ Calcule l'altitude maximale d'un entier n """
+    
+    return max(syracuse(n))
+
+print(altitudeMax(3))
+
+
+def plusGrandeAltitudeMax(n_max):
+    """ Détermine quel entier entre 1 et n_max a la plus grande altitude maximale """
+    
+    liste = []
+    
+    for i in range(1, n_max + 1):
+        liste.append(altitudeMax(i))
+    
+    return "L'entier", liste.index(max(liste)) + 1, "a la plus grande altitude maximale qui est de", (max(liste))
+
+print(plusGrandeAltitudeMax(10000))
